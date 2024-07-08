331bef9814426d5c2ccb973aef563b94903018e4
Nisha Patange <nishavpatange@gmail.com>
2024-07-08 18:09:34 -0400

    @6pm 7/8

--- code.js
+++ code.js
@@ -1,3 +1,16 @@
+let currentPhotoIndex = 0;
+let photos = [
+    {name: "entire-team", image: "home-ss/entire-team.jpg"},
+    {name: "teama-medals", image: "home-ss/teama-medals.jpg"},
+    {name: "Max+Michael", image: "Max+Michael.jpg"},
+    {name: "Nisha+Shyam", image: "Nisha+Shyam.jpg"},
+    {name: "Sarah", image: "Sarah.jpeg"}
+];
+
 function logoHome() {
     window.location.href = 'home.html';
+}
+
+function homeSsNext() {
+    console.log(photos);
 }
