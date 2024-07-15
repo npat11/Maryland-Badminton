4d53bf6241a79840ff0c6809ca3757759d6a7fb2
Nisha Patange <nishavpatange@gmail.com>
2024-07-15 11:47:48 -0400

    updates to socials to reduce code duplication

---
+++ socials.js
@@ -0,0 +1,12 @@
+document.write(`
+    <div id="socials">
+        <hr>
+        <ul class="social-links">
+            <li><a href="https://www.instagram.com/umdclubbadminton/" target="_blank">Instagram</a></li>
+            <li><a href="https://www.facebook.com/umdclubbadminton" target="_blank">Facebook</a></li>
+            <li><a href="https://discord.com/invite/w7XwATJueN" target="_blank">Discord</a></li>
+            <li><a href="mailto:info@umdclubbadminton.com" target="_blank">Email Us</a></li>
+        </ul>
+        <hr>
+    </div>
+`);
