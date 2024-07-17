d0ed03d64988aceab09d09ae91132564833b6255
Nisha Patange <nishavpatange@gmail.com>
2024-07-17 13:18:35 -0400

    update to avoid redundancy

---
+++ navbar.js
@@ -0,0 +1,15 @@
+document.write(`
+    <header>
+        <img class="logo" src="images/logo.jpg" alt="logo" onclick="logoHome()">
+
+        <h1>Maryland Badminton</h1>
+
+        <nav>
+            <ul class="nav-links">
+                <li><a href="new-members.html" target="_self">New Members</a></li>
+                <li><a href="team.html" target="_self">Competitive Team</a></li>
+                <li><a href="faq.html" target="_self">FAQ</a></li>
+            </ul>
+        </nav>
+    </header>
+`);
