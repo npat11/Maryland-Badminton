41fa5ac187d1ac24cf752d11d0d131d5346d9d21
Nisha Patange <nishavpatange@gmail.com>
2024-07-27 20:39:32 -0400

    Maryland Badminton Website v. 1.0 complete

--- code.js
+++ code.js
@@ -1,7 +1,9 @@
+//Click on logo, return to home page
 function logoHome() {
     window.location.href = 'index.html';
 }
 
+//FAQ open and close functionality for different questions
 document.addEventListener('DOMContentLoaded', function() {
     const questionButtons = document.querySelectorAll('.question');
 
@@ -12,17 +14,17 @@ document.addEventListener('DOMContentLoaded', function() {
 
             questionButtons.forEach(btn => {
                 const ans = btn.nextElementSibling;
+
+                //closing question that is opened if another question is clicked on
                 if (btn !== button && ans.classList.contains('active')) {
                     ans.classList.remove('active');
                     btn.classList.remove('active');
                 }
             });
 
+            //open question clicked on
             answer.classList.toggle('active');
             button.classList.toggle('active');
         });
     });
 });
-
-
-
