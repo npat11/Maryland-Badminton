af59b11f2341c596b294be1fd96c365f8f4ffc0d
Nisha Patange <nishavpatange@gmail.com>
2024-07-27 21:20:49 -0400

    page name updates

--- navbar.js
+++ navbar.js
@@ -8,7 +8,7 @@ document.write(`
             <ul class="nav-links">
                 <li><a href="new-members.html" target="_self">New Members</a></li>
                 <li><a href="team.html" target="_self">Competitive Team</a></li>
-                <li><a href="FAQ.html" target="_self">FAQ</a></li>
+                <li><a href="faq.html" target="_self">FAQ</a></li>
             </ul>
         </nav>
     </header>
